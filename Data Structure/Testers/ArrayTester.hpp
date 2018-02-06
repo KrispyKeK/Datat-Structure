c73efea7f13135bf08fbca10bd14731f034ef453
JJ <infernoj2581@gmail.com>
2018-02-06 08:24:37 -0700

    Created the tester group and added the arrayTester files

---
+++ Data Structure/Testers/ArrayTester.hpp
@@ -0,0 +1,14 @@
+//
+//  ArrayTester.hpp
+//  Data Structure
+//
+//  Created by Dela Cruz, Juan on 2/6/18.
+//  Copyright © 2018 Dela Cruz, Juan. All rights reserved.
+//
+
+#ifndef ArrayTester_hpp
+#define ArrayTester_hpp
+
+#include <stdio.h>
+
+#endif /* ArrayTester_hpp */
