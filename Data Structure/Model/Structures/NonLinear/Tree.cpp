fb98e7b4723fb34cc3b58dd97e794c324460764e
JJ <infernoj2581@gmail.com>
2018-03-29 08:07:55 -0600

    Created the binarySearchTree

--- Data Structure/Model/Structures/NonLinear/Tree.cpp
+++ Data Structure/Model/Structures/NonLinear/Tree.cpp
@@ -6,24 +6,4 @@
 //  Copyright © 2018 Dela Cruz, Juan. All rights reserved.
 //
 
-#include "Tree.hpp"
-#include "BinaryTreeNode.hpp"
-template <class Type>
-class Tree{
-protected:
-    BinaryTreeNode<Type> * root;
-public:
-    
-    virtual int getSize() = 0;
-    virtual int getHeight() = 0;
-    virtual bool isComplete() = 0;
-    virtual bool isBalanced() = 0;
-    //Data Members
-    virtual void insert(Type data) = 0;
-    virtual bool contains(Type data) = 0;
-    virtual void remove(Type data) = 0;
-    
-    virtual void inOrderTraversals() = 0;
-    virtual void preOrderTraversals() = 0;
-    virtual void postOrderTraversals() = 0;
-};
+
--- Data Structure/Model/Structures/NonLinear/Tree.hpp
+++ Data Structure/Model/Structures/NonLinear/Tree.hpp
@@ -10,5 +10,25 @@
 #define Tree_hpp
 
 #include <stdio.h>
-
+#include "Tree.hpp"
+#include "BinaryTreeNode.hpp"
+template <class Type>
+class Tree{
+protected:
+    BinaryTreeNode<Type> * root;
+public:
+    
+    virtual int getSize() = 0;
+    virtual int getHeight() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool isBalanced() = 0;
+    //Data Members
+    virtual void insert(Type data) = 0;
+    virtual bool contains(Type data) = 0;
+    virtual void remove(Type data) = 0;
+    
+    virtual void inOrderTraversals() = 0;
+    virtual void preOrderTraversals() = 0;
+    virtual void postOrderTraversals() = 0;
+};
 #endif /* Tree_hpp */
